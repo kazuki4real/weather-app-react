350255ed32bb4a9492abed968c45aa73b29a3a1d
kazuki4real <60100672+kazuki4real@users.noreply.github.com>
2021-04-22 20:41:57 +0900

    separated comp

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
+import WeatherMain from './WeatherMain'
+import Exception from './Exception'
 import TextField from '@material-ui/core/TextField';
-
+import CloudIcon from '@material-ui/icons/Cloud';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import BathtubIcon from '@material-ui/icons/Bathtub';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
 
 const Weather = () => {
@@ -11,7 +18,6 @@ const Weather = () => {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState('');
 
-
   const search = (e) => {
     if (e.key === "Enter") {
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=${API_KEY}`)
@@ -19,17 +25,76 @@ const Weather = () => {
         .then(result => {
           setWeather(result);
           setQuery('');
-          console.log(result)
+          // console.log(result)
         });
     }
   }
 
 
-
   const dateBuilder = () => {
-    let date = new Date().toString();
-    date = date.slice(0, 25)
-    return date;
+    switch (weather.timezone) {
+      case (-14400):
+        let newyork = new Date();
+        return newyork.toLocaleString('en-US', { timeZone: 'America/New_York' })
+        break;
+      case (32400):
+        let japan = new Date();
+        return japan.toLocaleString('en-US', { timeZone: 'Asia/Tokyo' })
+        break;
+      default:
+      case (-25200):
+        let westCoast = new Date();
+        return westCoast.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })
+        break;
+      case (-36000):
+        let Hawaii = new Date();
+        return Hawaii.toLocaleString('en-US', { timeZone: 'Pacific/Honolulu' })
+        break;
+      case (+3600):
+        let london = new Date();
+        return london.toLocaleString('en-US', { timeZone: 'Europe/London' })
+        break;
+      case (+36000):
+        let sydney = new Date();
+        return sydney.toLocaleString('en-US', { timeZone: 'Australia/Sydney' })
+        break;
+      case (+28800):
+        let Shanghai = new Date();
+        return Shanghai.toLocaleString('en-US', { timeZone: 'Asia/Shanghai' })
+        break;
+      case (+10800):
+        let mscow = new Date();
+        return mscow.toLocaleString('en-US', { timeZone: 'Europe/Moscow' })
+        break;
+    }
+  }
+
+  const weatherIcon = () => {
+    if (weather.weather[0].main === 'Clouds') {
+      return <CloudIcon
+        fontSize="inherit"
+        color="inherit" />
+    } else if (weather.weather[0].main === 'Rain') {
+      return <BeachAccessIcon
+        fontSize="inherit"
+        color="inherit" />
+    } else if (weather.weather[0].main === 'Clear') {
+      return <WbSunnyIcon
+        fontSize="inherit"
+        color="inherit" />
+    } else if (weather.weather[0].main === 'Snow') {
+      return <AcUnitIcon
+        fontSize="inherit"
+        color="inherit" />
+    } else if (weather.weather[0].main === 'Mist') {
+      return <BathtubIcon
+        fontSize="inherit"
+        color="inherit" />
+    } else {
+      return <InsertEmoticonIcon
+        fontSize="inherit"
+        color="inherit" />
+    }
   }
 
 
@@ -47,34 +112,16 @@ const Weather = () => {
           />
         </div>
         {(typeof weather.main != "undefined") ? (
-          <div className="location-box">
-            <div className="location">
-              {weather.name}, {weather.sys.country}
-            </div>
-            <div className="date">
-              {dateBuilder()}
-            </div>
-            <div className="weather-box">
-              <div className="temp">
-                {weather.main.temp}°
-                <div className="min-max">
-                  Highest：{weather.main.temp_max}°
-                </div>
-                <div className="min-max">
-                  Lowest：{weather.main.temp_min}°
-                </div>
-            </div>
-              <div className="weather">
-                {weather.weather[0].main}
-              </div>
-            </div>
-          </div>
+          <WeatherMain
+            query={query}
+            weather={weather}
+            dateBuilder={dateBuilder()}
+            weatherIcon={weatherIcon()}
+          />
         ) : (
-          <div className="place">
-            <p>
-              Hey! You want to know the weather across the world...?
-            </p>
-          </div>
+          <Exception
+          weather={weather}
+          />
         )}
       </main>
     </div>
